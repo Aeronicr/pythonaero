f1ec21cb8450f3da3979c7e6dacc6321c9b26f1c
Aeronicr <dmiriy.gon4arenko@gmail.com>
2020-09-14 19:43:11 +0300

    Building bot

--- bot.py
+++ bot.py
@@ -4,6 +4,18 @@ from config import settings
 import sys, traceback
 import os
 
+
+initial_extensions = ['fun', 'animals', 'info']
+bot = commands.Bot(command_prefix=get_prefix, description='A Rewrite Cog Example')
+
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print(f'Помилка завантаження додатків {extension}.', file=sys.stderr)
+            traceback.print_exc()
+
 def get_prefix(bot, message):
     """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
 
@@ -18,17 +30,6 @@ def get_prefix(bot, message):
     # If we are in a guild, we allow for the user to mention us or use any of the prefixes in our list.
     return commands.when_mentioned_or(*prefixes)(bot, message)
 
-
-initial_extensions = ['fun', 'animals', 'info']
-bot = commands.Bot(command_prefix=get_prefix, description='A Rewrite Cog Example')
-
-if __name__ == '__main__':
-    for extension in initial_extensions:
-        try:
-            bot.load_extension(extension)
-        except Exception as e:
-            print(f'Помилка завантаження додатків {extension}.', file=sys.stderr)
-            traceback.print_exc()
 
 @bot.event
 async def on_ready():
