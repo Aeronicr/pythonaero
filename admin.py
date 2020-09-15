e87e2348e9659bcdd8ee659295f68fd5ea9c3222
Aeronicr <dmitriy.gon4arenko@gmail.com>
2020-09-15 15:38:08 +0300

    good

--- admin.py
+++ admin.py
@@ -9,14 +9,14 @@ class Admin(commands.Cog, name="Info"):
     @commands.command(pass_context = True , aliases=['бан'])
     @commands.has_any_role("Батя","Славетний радник")
     async def ban (self, ctx, *, member:discord.User=None, reason =None):
-        if member == None or member == ctx.message.author:
+        if member == None or member == ctx.message.author or ctx.message.author.server_permissions.administrator:
             await ctx.channel.send("Ви не можете забанити себе або інших модераторів")
             return
         if reason == None:
             reason = "причину бану не вказано"
         message = f"Ви забанили користувача {ctx.guild.name} за {reason}"
         await member.send(message)
-        # await ctx.guild.ban(member, reason=reason)
+        await ctx.guild.ban(member, reason=reason)
         await ctx.channel.send(f"{member} забанено!")
 
 def setup(bot):
