98268fc9aaa52dbb907cbe51a9972d3aed2d4a22
Aeronicr <dmitriy.gon4arenko@gmail.com>
2020-09-15 18:34:41 +0300

    good

--- admin.py
+++ admin.py
@@ -14,6 +14,7 @@ class Admin(commands.Cog, name="Info"):
         # await ctx.channel.send(member.top_role.mention)
         # if member.top_role.mention == 'Славетний радник':
         #     await ctx.channel.send(member.top_role.mention)
+        member = ctx.author if not member else member
         roles = [role for role in member.roles]
         if roles == 'Славетний радник' or 'Батя':
                 await ctx.channel.send("Ви не можете заблокувати себе та інших модераторів")
