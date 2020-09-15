21096aaa7a2d0daf6089ea233d01bf2737b58ffd
Aeronicr <dmitriy.gon4arenko@gmail.com>
2020-09-15 17:38:59 +0300

    good

--- admin.py
+++ admin.py
@@ -10,16 +10,18 @@ class Admin(commands.Cog, name="Info"):
     @commands.command(pass_context = True , aliases=['бан'])
     @has_permissions(administrator=True, manage_messages=True, manage_roles=True)
     async def ban (self, ctx, *, member:discord.Member = None, reason = None):
-        if ctx.author.id == 339372925301948427:
-            # member == None or member == ctx.message.author
-            await ctx.channel.send("Ви не можете заблокувати себе")
-            return
-        if reason == None:
-            reason = "причину блокування не вказано"
-        message = f"Вас заблоковано модератором на {ctx.guild.name} за {reason}"
-        await member.send(message)
-        await ctx.guild.ban(member, reason=reason)
-        await ctx.channel.send(f"{member} заблоковано!")
+        roles = [role for role in member.roles]
+        await ctx.channel.send(roles)
+        # if ctx.author.id == 339372925301948427:
+        #     # member == None or member == ctx.message.author
+        #     await ctx.channel.send("Ви не можете заблокувати себе")
+        #     return
+        # if reason == None:
+        #     reason = "причину блокування не вказано"
+        # message = f"Вас заблоковано модератором на {ctx.guild.name} за {reason}"
+        # await member.send(message)
+        # await ctx.guild.ban(member, reason=reason)
+        # await ctx.channel.send(f"{member} заблоковано!")
 
     @ban.error
     async def ban_error(self, error, ctx):
