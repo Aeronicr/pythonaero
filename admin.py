76fd3f7f5b1662bdfdb716b5da6c8ae9295f915b
Aeronicr <dmitriy.gon4arenko@gmail.com>
2020-09-15 20:59:02 +0300

    good

--- admin.py
+++ admin.py
@@ -14,16 +14,18 @@ class Admin(commands.Cog, name="Info"):
         # await ctx.channel.send(member.top_role.mention)
         # if member.top_role.mention == 'Славетний радник':
         #     await ctx.channel.send(member.top_role.mention)
-        modRole = [r for r in ctx.guild.roles if r.name == "Славетний радник" or "Батя"][0]
-        if modRole.mention == member.top_role.mention:
-            await ctx.channel.send("Ви не можете заблокувати себе та інших модераторів")
-        else:
-            if reason == None:
-                reason = "причину блокування не вказано"
-            message = f"Вас заблоковано модератором на {ctx.guild.name} за {reason}"
-            await member.send(message)
-            await ctx.guild.ban(member, reason=reason)
-            await ctx.channel.send(f"{member} заблоковано!")
+        modRole = [r for r in ctx.guild.roles if r.name == "Славетний радник"][0]
+        await ctx.channel.send(modRole.mention)
+        await ctx.channel.send(member.top_role.mention)
+        # if modRole.mention == member.top_role.mention:
+        #     await ctx.channel.send("Ви не можете заблокувати себе та інших модераторів")
+        # else:
+        #     if reason == None:
+        #         reason = "причину блокування не вказано"
+        #     message = f"Вас заблоковано модератором на {ctx.guild.name} за {reason}"
+        #     await member.send(message)
+        #     await ctx.guild.ban(member, reason=reason)
+        #     await ctx.channel.send(f"{member} заблоковано!")
 
 def setup(bot):
     bot.add_cog(Admin(bot))
