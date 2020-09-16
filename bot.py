d555533aa51d9dd550502d7693795fef88e725ea
Aeronicr <dmitriy.gon4arenko@gmail.com>
2020-09-16 13:51:48 +0300

    good

--- bot.py
+++ bot.py
@@ -3,9 +3,6 @@ from discord.ext import commands
 import sys, traceback
 import os
 from discord.ext.commands import has_permissions
-import youtube_dl
-import ctypes
-import ctypes.util
 
 custom_prefixes = {}
 default_prefixes = ['.']
@@ -38,39 +35,6 @@ async def setprefix(ctx, *, prefixes=""):
     await ctx.send("Префікс бота успішно змінено!")
 
 bot.remove_command("help")
-
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
-}
-
-
-def endSong(guild, path):
-    os.remove(path)
-
-
-url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"  # link to your song on YouTube
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    file = ydl.extract_info(url, download=True)
-    guild = "731885002165583912"  # id of your server which you can get by right clicking on server name and clicking "Copy ID" (developer mode must be on)
-    path = str(file['title']) + "-" + str(file['id'] + ".mp3")
-
-channel1 = client.get_channel(755738347644911673)  # id of your channel (you get it like server id, but by right clicking on channel)
-voice_client = await channel1.connect()
-
-voice_client.play(discord.FFmpegPCMAudio(path), after=lambda x: endSong(guild, path))
-voice_client.source = discord.PCMVolumeTransformer(voice_client.source, 1)
-
-while voice_client.is_playing():  # waits until song ends
-    await asyncio.sleep(1)
-else:
-    await voice_client.disconnect()  # and disconnects
-    print("Disconnected")
-
 bot.run(os.environ['DISCORD_TOKEN'])
 
 
