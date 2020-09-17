e67c7980f826230da3804dc82f4c574b5a43ed6c
Aeronicr <dmitriy.gon4arenko@gmail.com>
2020-09-17 16:04:22 +0300

    good

--- bot.py
+++ bot.py
@@ -26,9 +26,23 @@ if __name__ == '__main__':
 @bot.event
 async def on_ready():
     activity = discord.Game(name="Захоплення світу")
-    channel = bot.get_channel(731885002668638242)
+    channel = bot.get_channel(755473910115336192)
     await bot.change_presence(status=discord.Status.idle, activity=activity)
     await channel.send("AeroBot долучився до серверу та готовий працювати!")
+
+    channel2 = bot.get_channel(755473910115336192)
+    Text= "Якщо ти пипаєш у борделі то став :slavetnyi_kreygasm:"
+    Moji = await client.send_message(channel2, Text)
+    await client.add_reaction(Moji, emoji=':slavetnyi_kreygasm:')
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    channel3 = client.get_channel('731885002668638242')
+    if reaction.message.channel.id != channel3
+    return
+    if reaction.emoji == ":slavetnyi_kreygasm:":
+      Role = discord.utils.get(user.server.roles, name="bot maker")
+      await client.add_roles(user, Role)
 
 @bot.command(aliases=['префікс'])
 @has_permissions(administrator=True, manage_messages=True, manage_roles=True)
