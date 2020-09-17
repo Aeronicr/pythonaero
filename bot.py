cd9cae49ee4c1bf276d2d3f29d2d9044b6966fb7
Aeronicr <dmitriy.gon4arenko@gmail.com>
2020-09-17 13:49:49 +0300

    good

--- bot.py
+++ bot.py
@@ -26,9 +26,9 @@ if __name__ == '__main__':
 @bot.event
 async def on_ready():
     activity = discord.Game(name="Захоплення світу")
-    channel = bot.get_channel(731885002668638242)
+    channel = bot.get_channel(731887251482607677)
     await bot.change_presence(status=discord.Status.idle, activity=activity)
-    await channel.send("Славетни бот влітає на славетний сервер!")
+    await channel.send("AeroBot доєднався до серверу!")
 
 @bot.command(aliases=['префікс'])
 @has_permissions(administrator=True, manage_messages=True, manage_roles=True)
