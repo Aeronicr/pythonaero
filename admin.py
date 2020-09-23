b247920f1eb1a34c7777fdaab3f0efcfd37f4bb0
Aeronicr <dmitriy.gon4arenko@gmail.com>
2020-09-23 18:58:33 +0300

    good

--- admin.py
+++ admin.py
@@ -54,11 +54,11 @@ class Admin(commands.Cog, name="Info"):
     async def unmute (self, ctx, member:discord.Member = None):
         modRole5 = [r for r in ctx.guild.roles if r.name == "Славетний троляка"][0]
         if modRole5.mention == member.top_role.mention:
-            # await ctx.channel.send("Ви не можете заглушити себе та інших модераторів")
-            await member.remove_roles(discord.utils.get(member.guild.roles, name='Троляка'))
-            await ctx.channel.send(f"Користувача {member} розглушено!")
-        else:
-            await ctx.channel.send("Користувач не ж заглушеним на даному сервері!")
+            await ctx.channel.send("Ви не можете заглушити себе та інших модераторів")
+        #     await member.remove_roles(discord.utils.get(member.guild.roles, name='Троляка'))
+        #     await ctx.channel.send(f"Користувача {member} розглушено!")
+        # else:
+        #     await ctx.channel.send("Користувач не ж заглушеним на даному сервері!")
 
     @commands.command(pass_context=True, aliases=['del', 'очистити'])
     async def clear(self, ctx, amount):
