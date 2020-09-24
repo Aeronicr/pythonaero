ecd7eef84b1ac9e7688596c8d6bb009bcd9bea28
Aeronicr <dmitriy.gon4arenko@gmail.com>
2020-09-24 20:09:15 +0300

    good

--- admin.py
+++ admin.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord.ext.commands import has_permissions, CheckFailure
 from discord.utils import get
 import asyncio
+import typing
 from typing import Optional
 
 
