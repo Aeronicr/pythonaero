21b612d919a7bc92b724e336ed48610245f2ea89
Aeronicr <dmitriy.gon4arenko@gmail.com>
2020-09-24 20:07:29 +0300

    good

--- admin.py
+++ admin.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord.ext.commands import has_permissions, CheckFailure
 from discord.utils import get
 import asyncio
+from typing import Optional
 
 
 class Admin(commands.Cog, name="Info"):
