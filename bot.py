5a8682e2389b3072c6a0aca30c849c6f3af9910b
Aeronicr <dmitriy.gon4arenko@gmail.com>
2020-09-25 12:24:36 +0300

    good

--- bot.py
+++ bot.py
@@ -32,13 +32,13 @@ async def on_ready():
     await channel.send(embed=embed)
     # await channel.send("AeroBot долучився до серверу та готовий працювати!")
 
-@bot.event
-async def on_member_join(member):
-     embed = discord.Embed(color=0x63ff52, title=':confetti_ball: Новий користувач долучився до серверу :confetti_ball:')
-    embed.set_thumbnail(url=member.avatar_url)
-    embed.add_field(name=f"Вітаю тебе, {member} на славетному сервері Славенія.", value="Сподіваюсь ти станеш частинкою даної спільноти. Перш ніж перейти до ближчого знайомства, пропоную тобі переглянути інформацію про даний сервер та правила у #славетний-закон та #славетні правила. Уважно прочитай усе, лише уважні отримають доступ до усіх каналів :)", inline=False)
-    embed.set_footer(text=f"Приємного спілкування {member}", icon_url=member.avatar_url)
-    await ctx.send(755473910115336192, embed=embed)
+# @bot.event
+# async def on_member_join(member):
+#      embed = discord.Embed(color=0x63ff52, title=':confetti_ball: Новий користувач долучився до серверу :confetti_ball:')
+#     embed.set_thumbnail(url=member.avatar_url)
+#     embed.add_field(name=f"Вітаю тебе, {member} на славетному сервері Славенія.", value="Сподіваюсь ти станеш частинкою даної спільноти. Перш ніж перейти до ближчого знайомства, пропоную тобі переглянути інформацію про даний сервер та правила у #славетний-закон та #славетні правила. Уважно прочитай усе, лише уважні отримають доступ до усіх каналів :)", inline=False)
+#     embed.set_footer(text=f"Приємного спілкування {member}", icon_url=member.avatar_url)
+#     await ctx.send(755473910115336192, embed=embed)
 
 @bot.event
 async def on_member_remove(member):
