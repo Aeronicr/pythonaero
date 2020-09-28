5470e904324a64e4137d0919faabc489d4755ef4
Aeronicr <dmitriy.gon4arenko@gmail.com>
2020-09-28 22:47:29 +0300

    good

--- bot.py
+++ bot.py
@@ -65,6 +65,8 @@ async def test(ctx):
     embed.add_field(name="Test", value="Test")
     msg = await ctx.send(embed=embed)
     emoji = discord.utils.get(bot.emojis, name='slavetnyi_dypa')
+    channel = bot.get_channel(755473910115336192)
+    channel.send('emoji')
     await msg.add_reaction(emoji)
 
 @bot.event
