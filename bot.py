390e7e44cdf8ae57d8535c58cae92bc915978d48
Aeronicr <dmitriy.gon4arenko@gmail.com>
2020-09-28 22:56:47 +0300

    good

--- bot.py
+++ bot.py
@@ -73,7 +73,7 @@ async def test(ctx):
 async def on_reaction_add(reaction, member):
     # if reaction.message.channel.id != '755473910115336192':
     #     return
-    if str(reaction.emoji) == ':slavetnyi_dypa:':
+    if reaction.emoji == ':slavetnyi_dypa:':
         channel = bot.get_channel(755473910115336192)
         await channel.send('Ok')
         await member.add_roles(discord.utils.get(member.guild.roles, name='Еротика'))
