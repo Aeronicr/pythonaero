6b6de4269e1df62615415e63e7b85adfe842f6b6
Aeronicr <dmitriy.gon4arenko@gmail.com>
2020-10-05 20:03:10 +0300

    good

--- admin.py
+++ admin.py
@@ -94,9 +94,10 @@ class Admin(commands.Cog, name="Info"):
         await ctx.send(modRole1)
         await ctx.send(modRole2)
         if modRole1.mention == member.top_role.mention or modRole2.mention == member.top_role.mention:
-            embed = discord.Embed(color=0xfc5821, title=f':bangbang: Ви не можете заглушити себе та інших модераторів, а також користувачів, що вже є заглушеними! :bangbang:')
-            embed.set_footer(text=f"Системне повідомлення для {ctx.author}", icon_url=ctx.author.avatar_url)
-            await ctx.send(embed=embed)
+            # embed = discord.Embed(color=0xfc5821, title=f':bangbang: Ви не можете заглушити себе та інших модераторів, а також користувачів, що вже є заглушеними! :bangbang:')
+            # embed.set_footer(text=f"Системне повідомлення для {ctx.author}", icon_url=ctx.author.avatar_url)
+            # await ctx.send(embed=embed)
+            await ctx.send("OK")
         else:
             ctx.send("Йди до дупи")
         # else:
