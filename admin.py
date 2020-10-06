b34f574cf284f3eb11881dc6c789eb168a743819
Aeronicr <dmitriy.gon4arenko@gmail.com>
2020-10-06 15:41:12 +0300

    good

--- admin.py
+++ admin.py
@@ -89,10 +89,11 @@ class Admin(commands.Cog, name="Info"):
     @commands.command(pass_context = True)
     async def one (self, ctx, member:discord.Member = None, reason=None):
         member = ctx.author if not member else member
-        role = [role for role in member.roles]
+        role = [role for role in member.roles][1:]
+        role2 = [role.mention for role in roles]
         # if [role for role in member.roles][-2:] == 
         await ctx.send(role)
-        await ctx.send (role==modRole)
+        await ctx.send(role2)
         # await ctx.send(member.top_role.mention == modRole1)
         # if modRole1 == member.top_role.mention:
         #     embed = discord.Embed(color=0xfc5821, title=f':bangbang: Ви не можете заглушити себе та інших модераторів, а також користувачів, що вже є заглушеними! :bangbang:')
