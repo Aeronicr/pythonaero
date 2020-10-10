7633608096e5b258959c99ea34150d9ab9cb9bd6
Aeronicr <dmitriy.gon4arenko@gmail.com>
2020-10-10 19:40:08 +0300

    good

--- admin.py
+++ admin.py
@@ -24,11 +24,11 @@ class Admin(commands.Cog, name="Admin"):
         if set(role_mod1).issubset(role_mod2):
             embed = discord.Embed(color=0xfc5821, title=f':bangbang: Ви не можете заблокувати модератора серверу! :bangbang:')
             embed.set_footer(text=f"Системне повідомлення для {ctx.author}", icon_url=ctx.author.avatar_url)
-            await ctx.send(embed=embed)
+            await(await ctx.send(embed=embed)).delete(delay=50)
         elif set(role_owner1).issubset(role_owner2):
             embed = discord.Embed(color=0xfc5821, title=f':bangbang: Ви не можете заблокувати власника серверу! :bangbang:')
             embed.set_footer(text=f"Системне повідомлення для {ctx.author}", icon_url=ctx.author.avatar_url)
-            await ctx.send(embed=embed)
+            await(await ctx.send(embed=embed)).delete(delay=50)
         else:
             if reason == None:
                 reason = "<причину блокування не вказано>"
@@ -38,7 +38,7 @@ class Admin(commands.Cog, name="Admin"):
             embed.set_thumbnail(url=member.avatar_url)
             embed.add_field(name=f"Користувача {member} заблоковано за {reason}!", value="Сподіваємось це буде уроком для решти.", inline=False)
             embed.set_footer(text=f"Викликано {ctx.author}", icon_url=ctx.author.avatar_url)
-            await ctx.send(embed=embed)
+            await(await ctx.send(embed=embed)).delete(delay=50)
             await ctx.guild.ban(member, reason=reason)
 
     @commands.command(pass_context = True , aliases=['кік', 'вигнати'])
@@ -54,11 +54,11 @@ class Admin(commands.Cog, name="Admin"):
         if set(role_mod1).issubset(role_mod2):
             embed = discord.Embed(color=0xfc5821, title=f':bangbang: Ви не можете вигнати модератора серверу! :bangbang:')
             embed.set_footer(text=f"Системне повідомлення для {ctx.author}", icon_url=ctx.author.avatar_url)
-            await ctx.send(embed=embed)
+            await(await ctx.send(embed=embed)).delete(delay=50)
         elif set(role_owner1).issubset(role_owner2):
             embed = discord.Embed(color=0xfc5821, title=f':bangbang: Ви не можете вигнати власника серверу! :bangbang:')
             embed.set_footer(text=f"Системне повідомлення для {ctx.author}", icon_url=ctx.author.avatar_url)
-            await ctx.send(embed=embed)
+            await(await ctx.send(embed=embed)).delete(delay=50)
         else:
             if reason == None:
                 reason = "<причину блокування не вказано>"
@@ -68,7 +68,7 @@ class Admin(commands.Cog, name="Admin"):
             embed.set_thumbnail(url=member.avatar_url)
             embed.add_field(name=f"Користувача {member} виключено із серверу за {reason}!", value="Сподіваємось це буде уроком для решти.", inline=False)
             embed.set_footer(text=f"Викликано {ctx.author}", icon_url=ctx.author.avatar_url)
-            await ctx.send(embed=embed)
+            await(await ctx.send(embed=embed)).delete(delay=50)
             await ctx.guild.kick(member, reason=reason)
 
     @commands.command(pass_context = True , aliases=['мют', 'заглушити'])
@@ -124,7 +124,7 @@ class Admin(commands.Cog, name="Admin"):
                 embed.set_thumbnail(url=member.avatar_url)
                 embed.add_field(name=f"Користувача {member} розглушено", value="Сподіваємось ви усвідомили свою помилку.", inline=False)
                 embed.set_footer(text=f"Викликано {ctx.author}", icon_url=ctx.author.avatar_url)
-                await(await ctx.send(embed=embed))..delete(delay=70)
+                await(await ctx.send(embed=embed)).delete(delay=50)
 
     @commands.command(pass_context = True , aliases=['анмют', 'розглушити'])
     @has_permissions(administrator=True, manage_messages=True, manage_roles=True)
@@ -140,22 +140,22 @@ class Admin(commands.Cog, name="Admin"):
         if set(role_mod1).issubset(role_mod2):
             embed = discord.Embed(color=0xfc5821, title=f':bangbang: Ви не можете розглушити модератора серверу! :bangbang:')
             embed.set_footer(text=f"Системне повідомлення для {ctx.author}", icon_url=ctx.author.avatar_url)
-            await ctx.send(embed=embed)
+            await(await ctx.send(embed=embed)).delete(delay=50)
         elif set(role_owner1).issubset(role_owner2):
             embed = discord.Embed(color=0xfc5821, title=f':bangbang: Ви не можете розглушити власника серверу! :bangbang:')
             embed.set_footer(text=f"Системне повідомлення для {ctx.author}", icon_url=ctx.author.avatar_url)
-            await ctx.send(embed=embed)
+            await(await ctx.send(embed=embed)).delete(delay=50)
         elif role_used.mention != member.top_role.mention:
             embed = discord.Embed(color=0xfc5821, title=f':bangbang: Користувач {member.name} не є заглушеним на даному сервері! :bangbang:')
             embed.set_footer(text=f"Системне повідомлення для {ctx.author}", icon_url=ctx.author.avatar_url)
-            await ctx.send(embed=embed)
+            await(await ctx.send(embed=embed)).delete(delay=50)
         else:
             await member.remove_roles(*role)
             embed = discord.Embed(color=0x63ff52, title=':white_check_mark: Знято покарання :white_check_mark:')
             embed.set_thumbnail(url=member.avatar_url)
             embed.add_field(name=f"Користувача {member} розглушено", value="Сподіваємось ви усвідомили свою помилку.", inline=False)
             embed.set_footer(text=f"Викликано {ctx.author}", icon_url=ctx.author.avatar_url)
-            await ctx.send(embed=embed)
+            await(await ctx.send(embed=embed)).delete(delay=50)
 
     @commands.command(pass_context=True, aliases=['del', 'очистити', 'чистити'])
     async def clear(self, ctx, amount):
@@ -167,13 +167,13 @@ class Admin(commands.Cog, name="Admin"):
 
         await channel.delete_messages(messages)
         while amount in range (1,5) or amount in range (21,25) or amount in range (31,35) or amount in range (41,45) or amount in range (51,55) or amount in range (61,65) or amount in range (71,75) or amount in range (81,85) or amount in range (91,95):
-            await(await ctx.send(f'{amount} повідомлення видалено.')).delete(delay=5)
+            await(await ctx.send(f'{amount} повідомлення видалено.')).delete(delay=15)
             break
         while amount in range(5,21) or amount in range(25,31) or amount in range(35,41) or amount in range(45,51) or amount in range(55,61) or amount in range(65,71) or amount in range(75,81) or amount in range(85,91) or amount in range(95,101):
-            await(await ctx.send(f'{amount} повідомлень видалено.')).delete(delay=5)
+            await(await ctx.send(f'{amount} повідомлень видалено.')).delete(delay=15)
             break
         if amount > 100:
-            await(await ctx.send(f'Повідомлень видалено: {amount}.')).delete(delay=5)
+            await(await ctx.send(f'Повідомлень видалено: {amount}.')).delete(delay=15)
         
 
 
