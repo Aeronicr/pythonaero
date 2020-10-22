a722b9ec62a528bd9f2db83a90b511832469f4d1
Aeronicr <dmitriy.gon4arenko@gmail.com>
2020-10-22 11:19:28 +0300

    good

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.idle, activity=activity)
     await channel.send("Бота перезавантажено")
     for guild in bot.guilds:
-        cursor.execute(f"SELECT guild_id FROM Guilds where id={guild.id}")
+        cursor.execute(f"SELECT guild_id FROM Guilds where guild_id={guild.id}")
         if cursor.fetchone()==None:
             cursor.execute(f"INSERT INTO Guilds VALUES ({guild.id}, '<@{guild.owner_id}>', '{guild.name}'")
         else:
